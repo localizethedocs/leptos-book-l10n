b416eec3fd6b987c92faf60f1f539d5ef13270db
ltdrobot <ltdrobot@localizethedocs.org>
2026-05-19 11:22:53 +0000

    Deploying to pages from @ localizethedocs/leptos-book-l10n@5c19aba236cd512b22399386f4a8599febd7742e 🚀

---
+++ ltd-config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",     "English"],
+    ["zh-cn",     "简体中文"],
+    ["zh-tw",     "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",      "Development"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",     "https://projects.localizethedocs.org"],
+    ["Crowdin",   "https://localizethedocs.crowdin.com/leptos-book-l10n"],
+    ["GitHub",    "https://github.com/localizethedocs/leptos-book-l10n"],
+    ["AtomGit",   "https://atomgit.com/localizethedocs/leptos-book-l10n"],
+    ["GitFlic",   "https://gitflic.ru/project/localizethedocs/leptos-book-l10n"],
+  ]
+};
